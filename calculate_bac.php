5f90c8ac6d994093cc00dab08932284d6f238b69
Pravat Nagarkoti <pravat.nagarkoti@gmail.com>
2024-08-16 09:44:45 -0700

    final changes

--- calculate_bac.php
+++ calculate_bac.php
@@ -1,9 +1,13 @@
 <?php
+
 //fetching data from form
-$alcohol_content = $_POST['alcohol_content'];
 $drinks = $_POST['drinks'];
 $weight = $_POST['weight'];
 $time_elapsed = $_POST['time_elapsed'];
+
+//calculating total alcohol consumption
+$alcohol_content_per_drink = $_POST['alcohol_content'];
+$total_alcohol_consumed = $drinks * $alcohol_content_per_drink;
 
 //setting gender_constant value for two genders
 if ($_POST['gender'] == "male") {
@@ -18,7 +22,9 @@ if ($_POST['unit'] == "kg") {
 
 
 //calculating BAC
-$BAC = ($alcohol_content * $drinks * 5.14) / ($weight * $gender_constant) - 0.015 * $time_elapsed;
+$BAC = ($total_alcohol_consumed * 5.14) / ($weight * $gender_constant) - 0.015 * $time_elapsed;
 
 //redirectig to main page along with a variable
 header("Location:/?data=$BAC");
+
+?>
